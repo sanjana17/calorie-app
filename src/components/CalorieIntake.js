218bacc8145eb49371f1eda3eddc1ea21a3fdac4
sanjana17 <navyasanjana4@gmail.com>
2018-08-18 18:26:47 -0400

    Bootstrap and material ui componets

--- src/components/CalorieIntake.js
+++ src/components/CalorieIntake.js
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 class CalorieIntake extends Component {
     render() {
         return (
-            <h1>CALORIE INTAKE</h1>
+            <div className="Calorie-Intake">
+                <h2 >Calorie Intake</h2>
+                <div class="input-group input-group-sm mb-3">
+                <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
+                </div>
+                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"/>
+                </div>
+            </div>
         );
     }
 }
