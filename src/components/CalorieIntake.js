23e6bbbd11581cd6c9c4b36ec96831e6f0550488
sanjana17 <navyasanjana4@gmail.com>
2018-08-21 21:57:33 -0400

    resolved conflicts

--- src/components/CalorieIntake.js
+++ src/components/CalorieIntake.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
+import './CalorieIntake.css'
 
 class CalorieIntake extends Component {
     render() {
         return (
             <div className="Calorie-Intake">
                 <h2 >Calorie Intake</h2>
-                <div className="input-group input-group-sm mb-3">
-                <div className="input-group-prepend">
-                <span className="input-group-text" id="inputGroup-sizing-sm">Small</span>
-                </div>
-                <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"/>
-                </div>
+                    <div className="row">
+                    <div className="col-2"></div>
+                    <form className="col-8">
+                        <div className="row">
+                        <div class="group" className="col-3">
+                            <input type="Item" required/>
+                            <span class="highlight"></span>
+                            <span class="bar"></span>
+                            <label>Food Item</label>
+                        </div>
+                        &nbsp;&nbsp;
+                        <div class="group" className="col-3">
+                            <input type="quantity" required/>
+                            <span class="highlight"></span>
+                            <span class="bar"></span>
+                            <label>Quantity</label>
+                        </div>
+                        <span className="col-1"></span>
+                        <div class="group" className="col-3">
+                            <input type="units" required/>
+                            <span class="highlight"></span>
+                            <span class="bar"></span>
+                            <label>units</label>
+                        </div>
+                        </div>
+                        <button type="button" class="btn btn-primary">Primary</button>
+                    </form>
+                    <div className="col-2"></div>
+                    </div>
             </div>
         );
     }
